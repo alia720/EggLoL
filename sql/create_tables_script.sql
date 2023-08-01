8d81c5c678f5ebfb67487a4b82deed0a3a0cd3ed
Maitra Patel <10maitra22@gmail.com>
2023-07-31 18:11:33 -0600

    script
    added the SQL script that will be used to create the PostgreSQL tables

---
+++ sql/create_tables_script.sql
@@ -0,0 +1,17 @@
+CREATE TABLE lol_profile (
+
+    profile_uuid UUID NOT NULL PRIMARY KEY,
+    region VARCHAR(4) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    main_champion VARCHAR(14) NOT NULL,
+    rank VARCHAR(11),
+
+);
+
+CREATE TABLE discord_user (
+
+    discord_user_id BIGINT NOT NULL PRIMARY KEY,
+    profile_uuid UUID REFERENCES lol_profile (profile_uuid),
+    UNIQUE (profile_uuid)
+
+)
